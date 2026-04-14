fe802818c70cf46cbef6df4cd792b601d715d7b7
Evelyn0Cha <evelyncha42@gmail.com>
2026-04-14 13:55:10 -0500

    today sohail taught me how to transfer files to another repositi-roryrohoeijoiawho;fnvcsalhf o;eashoudpieabdogpendvdcjx

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/transfer/PracticeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/transfer/PracticeAuto.java
@@ -17,21 +17,30 @@ public class PracticeAuto extends OpMode {
     private int pathState;
 
     //poses
-    private final Pose startPose = new Pose(56,8, Math.toRadians(90));
+    private final Pose startPose = new Pose(50,2, Math.toRadians(90));
 
     private final Pose setIntake1 = new Pose(36, 35, Math.toRadians(180));
-    private final Pose intake1 = new Pose(8, 35, Math.toRadians(180));
+    private final Pose intake1 = new Pose(-5, 35, Math.toRadians(180));
 
-    private final Pose control1 = new Pose(53.5, 44);
-    private final Pose setIntake2 = new Pose(38, 58.5, Math.toRadians(180));
-    private final Pose intake2 = new Pose(8, 58.5, Math.toRadians(180));
-    private final Pose control2 = new Pose(56.5, 77);
+    private final Pose control1 = new Pose(60, 26);
+
+    private final Pose setIntake2 = new Pose(43, 61.5, Math.toRadians(180));
+    private final Pose intake2 = new Pose(-5, 61.5, Math.toRadians(180));
+
+    private final Pose control2 = new Pose(74.5, 72);
+
+    private final Pose gate = new Pose(-1, 69, Math.toRadians(180));
+
+    private final Pose controlGate = new Pose(24, 59.5, Math.toRadians(180));
+
     private final Pose setIntake3 = new Pose(39, 83, Math.toRadians(180));
-    private final Pose intake3 = new Pose(14, 83, Math.toRadians(180));
+    private final Pose intake3 = new Pose(4, 83, Math.toRadians(180));
 
+    private final Pose control3 = new Pose(60, 81);
 
-    private final Pose launchPose = new Pose(38.5, 108, Math.toRadians(140));
+    private final Pose launchPose = new Pose(40, 105, Math.toRadians(140)); // adjust later
     private final Pose endPose = new Pose(54, 81, Math.toRadians(180));
+
 
 
     //paths
@@ -41,9 +50,12 @@ public class PracticeAuto extends OpMode {
     private PathChain moveThree;
     private PathChain moveFour;
     private PathChain moveFive;
+    private PathChain openGate;
     private PathChain moveSix;
     private PathChain moveSeven;
     private PathChain moveEight;
+    private PathChain moveNine;
+    private PathChain end;
 
     //build paths
     public void buildPaths(){
@@ -56,25 +68,120 @@ public class PracticeAuto extends OpMode {
                 .setLinearHeadingInterpolation(setIntake1.getHeading(), intake1.getHeading())
                 .build();
         moveThree = follower.pathBuilder()
-                .addPath(new BezierCurve(intake1, launchPose))
+                .addPath(new BezierCurve(intake1, control1, launchPose))
                 .setLinearHeadingInterpolation(intake1.getHeading(), launchPose.getHeading())
+                .build();
+        moveFour = follower.pathBuilder()
+                .addPath(new BezierLine(launchPose, setIntake2))
+                .setLinearHeadingInterpolation(launchPose.getHeading(), setIntake2.getHeading())
+                .build();
+        moveFive = follower.pathBuilder()
+                .addPath(new BezierLine(setIntake2, intake2))
+                .setLinearHeadingInterpolation(setIntake2.getHeading(), intake2.getHeading())
+                .build();
+        openGate = follower.pathBuilder()
+                .addPath(new BezierCurve(intake2, controlGate, gate))
+                .setLinearHeadingInterpolation(intake2.getHeading(), gate.getHeading())
+                .build();
+        moveSix = follower.pathBuilder()
+                .addPath(new BezierCurve(gate, control2, launchPose))
+                .setLinearHeadingInterpolation(gate.getHeading(), launchPose.getHeading())
+                .build();
+        moveSeven = follower.pathBuilder()
+                .addPath(new BezierCurve(launchPose, control3, setIntake3))
+                .setLinearHeadingInterpolation(launchPose.getHeading(), setIntake3.getHeading())
+                .build();
+        moveEight = follower.pathBuilder()
+                .addPath(new BezierLine(setIntake3, intake3))
+                .setLinearHeadingInterpolation(setIntake3.getHeading(), intake3.getHeading())
+                .build();
+        moveNine = follower.pathBuilder()
+                .addPath(new BezierLine(intake3, launchPose))
+                .setLinearHeadingInterpolation(intake3.getHeading(), launchPose.getHeading())
+                .build();
+        end = follower.pathBuilder()
+                .addPath(new BezierLine(launchPose, endPose))
+                .setLinearHeadingInterpolation(launchPose.getHeading(), endPose.getHeading())
                 .build();
     }
 
     public void pathUpdate() {
 
+        follower.setMaxPower(0.5);
         switch(pathState) {
             case 0:
                 follower.followPath(moveOne);
-                setPathState(-1);
+                setPathState(1);
                 break;
+            case 1:
+                if(!follower.isBusy()) {
+                    follower.followPath(moveTwo);
+                    setPathState(2);
+                    break;
+                }
+            case 2:
+                if(!follower.isBusy()) {
+                    follower.followPath(moveThree);
+                    setPathState(3);
+                    break;
+                }
+            case 3:
+                if(!follower.isBusy()) {
+                    follower.followPath(moveFour);
+                    setPathState(4);
+                    break;
+                }
+            case 4:
+                if(!follower.isBusy()) {
+                    follower.followPath(moveFive);
+                    setPathState(5);
+                    break;
+                }
+            case 5:
+                if(!follower.isBusy()) {
+                    follower.followPath(openGate);
+                    setPathState(6);
+                    break;
+                }
+            case 6:
+                if(!follower.isBusy()) {
+                    follower.followPath(moveSix);
+                    setPathState(7);
+                    break;
+                }
+            case 7:
+                if(!follower.isBusy()) {
+                    follower.followPath(moveSeven);
+                    setPathState(8);
+                    break;
+                }
+            case 8:
+                if(!follower.isBusy()) {
+                    follower.followPath(moveEight);
+                    setPathState(9);
+                    break;
+                }
+            case 9:
+                if(!follower.isBusy()) {
+                    follower.followPath(moveNine);
+                    setPathState(10);
+                    break;
+                }
+            case 10:
+                if(!follower.isBusy()) {
+                    follower.followPath(end);
+                    setPathState(-1);
+                    break;
+                }
         }
 
-
+/*
         if(pathState == 0) {
             follower.followPath(moveOne);
             setPathState(-1);
         }
+
+ */
     }
 
     private void setPathState(int state) {
@@ -84,8 +191,9 @@ public class PracticeAuto extends OpMode {
     @Override
     public void init() {
         follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(startPose);
         buildPaths();
+        follower.setStartingPose(startPose);
+
     }
 
     @Override
@@ -97,5 +205,10 @@ public class PracticeAuto extends OpMode {
     public void loop() {
         follower.update();
         pathUpdate();
+    }
+
+    @Override
+    public void stop() {
+
     }
 }
